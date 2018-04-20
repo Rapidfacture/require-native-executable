844f4be64cf6e72afd7ba737ff285c76a6c7d8c2
Felix <ff@rapidfacture.com>
2018-04-20 11:13:26 +0200

    eslint index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const commandExists = require('command-exists');
 const commandExistsSync = commandExists.sync;
 
-class NativeExecutableMissingError extends Error {
-}
+class NativeExecutableMissingError extends Error {}
 
-function _err(exeName) {
+function _err (exeName) {
     const err = new NativeExecutableMissingError(`The native executable '${exeName}' was not found on your system but is required to load this application.`);
     err.exeName = exeName;
     return err;
@@ -14,12 +13,12 @@ module.exports = {
     /**
      * Check if a native executable exist - if not, throw an error
      */
-    requireNativeExecutableSync: function(exeName) {
-        if(!commandExistsSync(exeName)) {
+    requireNativeExecutableSync: function (exeName) {
+        if (!commandExistsSync(exeName)) {
             throw _err(exeName);
         }
     },
-    requireNativeExecutable: function(exeName) {
+    requireNativeExecutable: function (exeName) {
         return new Promise((resolve, reject) => {
             commandExists(exeName).then(resolve).catch(() => {
                 reject(_err(exeName));
@@ -27,4 +26,4 @@ module.exports = {
         });
     },
     NativeExecutableMissingError: NativeExecutableMissingError
-}
+};
